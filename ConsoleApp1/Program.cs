86bb3435710ba66dac7bafc0e91b14d95a4e1fdf
Jakub Kogut <w67245@student.wsiz.edu.pl>
2024-10-24 14:59:26 +0200

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zadanie1
+//using System;
+
+//class Program
+//{
+//  static void Main()
+//{
+//  Console.WriteLine("Podaj liczbe:");
+//int liczba = int.Parse(Console.ReadLine());
+
+//        if (liczba % 2 == 0)
+//      {
+//        Console.WriteLine("Liczba {0} jest parzysta", liczba);
+//  }
+//else
+//{
+//  Console.WriteLine("Liczba {0} jest nieparzysta", liczba);
+//}
+//}
+//}
+
+//zadanie2
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbe n:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Parzyste liczby od 1 do {0}", n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
