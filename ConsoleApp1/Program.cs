023c82a00ef4e33f2e49030718593006416e6250
Jakub Kogut <w67245@student.wsiz.edu.pl>
2024-10-24 14:59:52 +0200

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,23 +20,36 @@
 //}
 
 //zadanie2
-using System;
+//using System;
 
-class Program
+//class Program
+//{
+//  static void Main()
+//{
+//  Console.WriteLine("Podaj liczbe n:");
+//int n = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Parzyste liczby od 1 do {0}", n);
+
+//        for (int i = 1; i <= n; i++)
+//      {
+//        if (i % 2 == 0)
+//      {
+//        Console.WriteLine(i);
+//  }
+// }
+// }
+//}
+
+//zadanie3
+
+while (true)
 {
-    static void Main()
-    {
-        Console.WriteLine("Podaj liczbe n:");
-        int n = int.Parse(Console.ReadLine());
+    Console.WriteLine("Wybierz opcje");
+    Console.WriteLine("1. Parzyste");
+    Console.WriteLine("2. Ile licz parzystych");
+    Console.WriteLine("3. Silnia");
+    Console.WriteLine("0. Wyjscie");
 
-        Console.WriteLine("Parzyste liczby od 1 do {0}", n);
 
-        for (int i = 1; i <= n; i++)
-        {
-            if (i % 2 == 0)
-            {
-                Console.WriteLine(i);
-            }
-        }
-    }
 }
