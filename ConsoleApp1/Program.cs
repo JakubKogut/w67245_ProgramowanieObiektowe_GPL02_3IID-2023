bba55482afd8d91d2b5948e064d87e31be83cd3c
Jakub Kogut <w67245@student.wsiz.edu.pl>
2024-11-14 14:55:09 +0100

    lab2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -43,13 +43,96 @@
 
 //zadanie3
 
-while (true)
-{
-    Console.WriteLine("Wybierz opcje");
-    Console.WriteLine("1. Parzyste");
-    Console.WriteLine("2. Ile licz parzystych");
-    Console.WriteLine("3. Silnia");
-    Console.WriteLine("0. Wyjscie");
+//while (true)
+//{
+//  Console.WriteLine("Wybierz opcje");
+//Console.WriteLine("1. Parzyste");
+// Console.WriteLine("2. Ile licz parzystych");
+//Console.WriteLine("3. Silnia");
+//Console.WriteLine("0. Wyjscie");
 
+
+//}
+
+//zadanie 1 lab 2-3
+public class Person
+{
+    private string name;
+    private string surname;
+    private int age;
+    private string pesel;
+
+    public Person(string name, string surname, int wiek, string pesel)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.pesel = pesel;
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Surname
+    {
+        get { return surname; }
+        set { surname = value; }
+    }
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value < 0) age = 0;
+            else age = value;
+
+        }
+
+    }
+    public string Pesel
+    {
+        get { return pesel; }
+    }
+
+    public void IntroduceYourself()
+    {
+        Console.WriteLine($"My name is {name} {surname} and my age is {age}.");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person("Jan", "Kowalski", 25 , "123456789");
+        person.IntroduceYourself();
+    }
+}
+
+
+public class zadanie2
+{
+    public double number;
+
+    public void Add(double value)
+    {
+        this.number += value;
+    }
+    public void WriteValue()
+        { Console.WriteLine(number); }
+}
+
+public class zadanie3
+{
+    public int[] Numbers { get; set; } = new int[] { 10, 20, 30 };
+
+    public int Sum() { return Numbers.Sum(); }
+
+
+    public double SumDiv3() { return Numbers.Where(x => x % 3 == 0).Sum(); }
+    public int Count() { return Numbers.Count(); }
+
+    public void Display(int low, int high);
 
 }
